259d77dcff36b387f695e382a7fb00dc01d1ba3c
Harshal Patel <102987398+Harshal1414@users.noreply.github.com>
2022-10-12 20:17:23 +0530

    Starters 60

---
+++ CC_Tax_in_Chefland.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class CC_Tax_in_Chefland {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0; i<t; i++){
+            int x = sc.nextInt();
+            if(x>100){
+                System.out.println(x-10);
+            }
+            else{
+                System.out.println(x);
+            }
+        }
+    }
+}
