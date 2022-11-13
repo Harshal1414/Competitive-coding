bf0caa40cbeac54ac854be4737745f38e2d2d29e
Harshal Patel <102987398+Harshal1414@users.noreply.github.com>
2022-11-13 15:48:57 +0530

    CC_NOV_Long_Challenge

---
+++ CC_Time_Complexity.java
@@ -0,0 +1,19 @@
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int i = 0; i < t; i++){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    if(x > y){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+	}
+}
